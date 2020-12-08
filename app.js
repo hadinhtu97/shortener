e32eb7023df7758033bdf6038a1e419acea4a6fe
hadinhtu97 <hadinhtu97@gmail.com>
2020-12-08 19:17:57 +0700

    update index, add css

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, () => {
     console.log('Your app is listening on port ' + port);
