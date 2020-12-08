33d414a54a4a95c80ca0d45ccb61a2061f20e551
Tu Ha <hadinhtu97@gmail.com>
2020-12-08 14:30:44 +0000

    update valid url

--- app.js
+++ app.js
@@ -14,58 +14,61 @@ app.get('/', (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use(express.static(__dirname + "/public"));
 
 let listUrl = [
-    {
-        original_url: "https://www.freecodecamp.org",
-        short_url: 1
-    },
-    {
-        original_url: "https://www.google.com",
-        short_url: 2
-    }
+  {
+    original_url: "https://www.freecodecamp.org",
+    short_url: 1
+  },
+  {
+    original_url: "https://www.google.com",
+    short_url: 2
+  }
 ];
 
 app.post('/api/shorturl/new', (req, res) => {
-    let url = req.body.url;
-    let httpRegex = /^https:\/\//;
-    if (httpRegex.test(url)) {
-        let rest = url.replace(httpRegex, '');
-        if (rest === '') {
-            res.json({ error: 'invalid url' });
-        } else {
-            dns.lookup(rest, (err) => {
-                if (err) {
-                    res.json({ error: 'invalid url' });
-                } else {
-                    if (listUrl.filter(li => li.original_url === url).length == 0) {
-                        listUrl.push({
-                            original_url: url,
-                            short_url: listUrl.length + 1
-                        })
-                        res.json(listUrl.filter(li => li.original_url === url)[0])
-                    } else {
-                        res.json(listUrl.filter(li => li.original_url === url)[0])
-                    }
-                }
-            })
-        }
+  let url = req.body.url;
+  let httpRegex = /^https:\/\//;
+  if (httpRegex.test(url)) {
+    let rest = url.replace(httpRegex, '');
+    if (rest === '') {
+      res.json({ error: 'invalid url' });
     } else {
-        res.json({ error: 'invalid url' });
+      let regex = /\/\w+/g;
+      rest = rest.replace(regex, '');
+      console.log(rest);
+      dns.lookup(rest, (err) => {
+        if (err) {
+          res.json({ error: 'invalid url' });
+        } else {
+          if (listUrl.filter(li => li.original_url === url).length == 0) {
+            listUrl.push({
+              original_url: url,
+              short_url: listUrl.length + 1
+            })
+            res.json(listUrl.filter(li => li.original_url === url)[0])
+          } else {
+            res.json(listUrl.filter(li => li.original_url === url)[0])
+          }
+        }
+      })
     }
+  } else {
+    res.json({ error: 'invalid url' });
+  }
 })
 
 app.get('/api/shorturl/:short_url', (req, res) => {
-    let nonNumberRegex = /\D/g;
-    if (nonNumberRegex.test(req.params.short_url)) {
-        res.json({ error: "Wrong format" })
+  let nonNumberRegex = /\D/g;
+  if (nonNumberRegex.test(req.params.short_url)) {
+    res.json({ error: "Wrong format" })
+  } else {
+    if (listUrl.filter(li => li.short_url == req.params.short_url).length == 0) {
+      res.json({ error: "No short URL found for the given input" })
     } else {
-        if (listUrl.filter(li => li.short_url == req.params.short_url).length == 0) {
-            res.json({ error: "No short URL found for the given input" })
-        } else {
-            res.redirect(listUrl.filter(li => li.short_url == req.params.short_url)[0].original_url)
-        }
+      res.redirect(listUrl.filter(li => li.short_url == req.params.short_url)[0].original_url)
     }
+  }
 })
 
 app.listen(port, () => {
-    console.log('Your app is listening on port ' + port);
+  console.log('Your app is listening on port ' + port);
 })
